964204247ea9e4699a93437e63467a650ebdda58
crystalDf <chendong333@gmail.com>
2017-05-11 23:32:53 +0800

    Chapter 06 DatabaseTest

--- app/src/main/java/com/star/databasetest/MyDatabaseHelper.java
+++ app/src/main/java/com/star/databasetest/MyDatabaseHelper.java
@@ -7,6 +7,21 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
+    public static final String TABLE_BOOK = "book";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_AUTHOR = "author";
+    public static final String COLUMN_PRICE = "price";
+    public static final String COLUMN_PAGES = "pages";
+    public static final String COLUMN_NAME = "name";
+
+    public static final String CREATE_BOOK = "create table " + TABLE_BOOK + " (" +
+            COLUMN_ID + " integer primary key autoincrement, " +
+            COLUMN_AUTHOR + " text, " +
+            COLUMN_PRICE + " real " +
+            COLUMN_PAGES + " integer, " +
+            COLUMN_NAME + " text" +
+            ")";
+
     public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
