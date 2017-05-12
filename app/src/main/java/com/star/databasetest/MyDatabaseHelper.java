ec6bac3bd952f28006c8561ec89bb5ab33d4e374
crystalDf <chendong333@gmail.com>
2017-05-12 23:58:52 +0800

    Chapter 06 DatabaseTest

--- app/src/main/java/com/star/databasetest/MyDatabaseHelper.java
+++ app/src/main/java/com/star/databasetest/MyDatabaseHelper.java
@@ -3,6 +3,7 @@ package com.star.databasetest;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 
 public class MyDatabaseHelper extends SQLiteOpenHelper {
@@ -26,11 +27,15 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
+
+        mContext = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        db.execSQL(CREATE_BOOK);
+        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_LONG).show();
     }
 
     @Override
