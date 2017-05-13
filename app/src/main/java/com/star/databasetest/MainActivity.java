b07fdb296ce586d850557bc4383742de341cfa61
crystalDf <chendong333@gmail.com>
2017-05-13 23:46:18 +0800

    Chapter 06 DatabaseTest

--- app/src/main/java/com/star/databasetest/MainActivity.java
+++ app/src/main/java/com/star/databasetest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.star.databasetest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private MyDatabaseHelper mMyDatabaseHelper;
     private Button mCreateDatabaseButton;
+    private Button mInsertDataButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mMyDatabaseHelper = new MyDatabaseHelper(this, DATABASE_BOOK_STORE, null, 2);
+
         mCreateDatabaseButton = (Button) findViewById(R.id.create_database);
         mCreateDatabaseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mMyDatabaseHelper.getWritableDatabase();
+            }
+        });
+
+        mInsertDataButton = (Button) findViewById(R.id.insert_data);
+        mInsertDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SQLiteDatabase sqLiteDatabase = mMyDatabaseHelper.getWritableDatabase();
+
+                ContentValues contentValues = new ContentValues();
+
+                contentValues.put(MyDatabaseHelper.COLUMN_NAME, "The Da Vinci Code");
+                contentValues.put(MyDatabaseHelper.COLUMN_AUTHOR, "Dan Brown");
+                contentValues.put(MyDatabaseHelper.COLUMN_PAGES, 454);
+                contentValues.put(MyDatabaseHelper.COLUMN_PRICE, 16.96);
+
+                sqLiteDatabase.insert(MyDatabaseHelper.TABLE_BOOK, null, contentValues);
+
+                contentValues.clear();
+
+                contentValues.put(MyDatabaseHelper.COLUMN_NAME, "The Lost Symbol");
+                contentValues.put(MyDatabaseHelper.COLUMN_AUTHOR, "Dan Brown");
+                contentValues.put(MyDatabaseHelper.COLUMN_PAGES, 510);
+                contentValues.put(MyDatabaseHelper.COLUMN_PRICE, 19.95);
+
+                sqLiteDatabase.insert(MyDatabaseHelper.TABLE_BOOK, null, contentValues);
+
             }
         });
     }
